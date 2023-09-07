f5ed303751d4cc0aaab8e843d5bfa843be75aa15
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2023-09-07 09:52:10 +0000

    Create taskp18.py

---
+++ taskp18.py
@@ -0,0 +1,29 @@
+#task 1 page 18
+age=16
+print(age,"what a great age")
+x=2.4
+y=6.2
+#task 2 page 18
+print("sum:",x+y,"diffrence:",x-y,"product:",x*y)
+#task 3 page 18
+answer= (x-y)
+answer= round(answer,2)
+print(answer)
+
+#task 4 page 19
+fahrenheit=-32
+mystring="celsius"
+answer=((5/9)*(fahrenheit))
+answer=round(answer,0)
+print (answer,mystring)
+#task 6 page 19
+h=6
+r=2
+pi=3.14
+volume=(pi*r**2*h)
+print(volume)
+totalLiquid=350
+total=(totalLiquid/volume)
+total=round(total,0)
+
+
