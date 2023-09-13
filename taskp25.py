bddf53716f49b98d5e63965c22d9d9fe38818416
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2023-09-13 22:16:58 +0100

    Add files via upload

---
+++ taskp25.py
@@ -0,0 +1,28 @@
+#task1 page25
+artist=input("please name your favourite artist:")
+print(artist,"is a great artist" )
+#task3 page25
+# seconds=(input("please give me a five digit number"))
+# print=seconds
+#COME BACK LATER
+#task5 page25
+num1=input("pick a character from the keybored")
+num3=input("pick a character from the keybored")
+num2=input("pick a character from the keybored")
+num4=input("pick a character from the keybored")
+num5=input("pick a character from the keybored")
+ascii=ord(num1)
+print(type(ascii))
+print("the decmial values for num1 is: ",ascii)
+ascii=ord(num2)
+print(type(ascii))
+print("the decmial values for num2 is: ",ascii)
+ascii=ord(num3)
+print(type(ascii))
+print("the decmial values for num3 is: ",ascii)
+ascii=ord(num4)
+print(type(ascii))
+print("the decmial values for num4 is: ",ascii)
+ascii=ord(num5)
+print(type(ascii))
+print("the decmial values for num5 is: ",ascii)
