bc92c904787d23477d723c0c515409ba993c335a
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2023-09-20 18:17:13 +0100

    Update taskp31.py

--- taskp31.py
+++ taskp31.py
@@ -1,27 +1,24 @@
-# a=True
-# print(a)
-# luskcafes=5
-# rushcafes=8
-# print(luskcafes>rushcafes)
-# print((luskcafes==6)and(rushcafes==8))
-# answer=input("do you have a loyalty card?c(Y/N)")
-# if answer=="y":
-#     print("please scan your card.")
-# else:
-#     print("do you want to sight up for one?")
-# 
-# paytype=input("do you want to pay with card cash or coupon?")
-# if paytype=="cash":
-#     print("insert cash")
-# elif paytype=="card":
-#         print("insert card into machine.")
-# else:
-#     print("coupons are only accepted at customer service.")
+ a=True
+ print(a)
+ luskcafes=5
+ rushcafes=8
+ print(luskcafes>rushcafes)
+ print((luskcafes==6)and(rushcafes==8))
+ answer=input("do you have a loyalty card?c(Y/N)")
+ if answer=="y":
+     print("please scan your card.")
+ else:
+     print("do you want to sight up for one?")
+ 
+ paytype=input("do you want to pay with card cash or coupon?")
+ if paytype=="cash":
+     print("insert cash")
+ elif paytype=="card":
+         print("insert card into machine.")
+ else:
+     print("coupons are only accepted at customer service.")
             
 #task 4 page 31
-# A=ticket to dundrum shopping centre
-# B=ticket to tallaght
-# C=ticket to broombridge
 ticket=input(" please enter your voucher code")
 if ticket=="A":
     print("congratulations you won a ticket to dudrum shopping centre")
