9f355945b819614a73f0b70338c6bc241712730f
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2023-11-16 11:05:19 +0000

    Add files via upload

---
+++ for_loops.py
@@ -0,0 +1,55 @@
+# 
+# #one arguemt
+# for i in range(5):
+#     print(i)
+# #two arguments
+# print("loop two")
+# for i in range(1,5):
+#     print(i)
+# #3 argument
+# print("loop three")
+# for i in range(1,20,2):
+#     print(i)
+# #going back
+# print("loop four")
+# for i in range(20,1,-1):
+#     print(i)
+# 
+# sentence = input("enter a sentence:")
+# spacecount = 0
+# for i in sentence:
+#     if i ==" ":
+#         spacecount+=1
+# wordcount = spacecount+1
+# print("spaces", spacecount)
+# print("words", wordcount)
+# 
+# counter=0
+# for i range(100):
+#     counter=+5
+# print(counter)
+
+#task1 pg37
+for i in range(1,11):
+    print (i)
+
+counter= 0
+while counter<=10:
+    print (counter)
+    counter+=1
+
+#task2 pg37
+number= int(input("please enter a number"))
+for i in range(1,number,2):
+    print(i)
+    
+#task3 pg37
+sententce= (input("please enter a sentence"))
+for i in 
+
+    
+    
+              
+    
+
+    
