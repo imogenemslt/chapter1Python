2cf1ab9786cdaa85bd2d87e41ebef69e1b6f9f7c
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2024-05-21 16:57:36 +0000

    Add files via upload

---
+++ summer test.py
@@ -0,0 +1,35 @@
+# #task 1
+# students = [45,97,9,10,8,85,64,56,1,4,25,36,78]
+# print(students)
+# lengthlist=len(students)
+# print("there are",lengthlist,"students in the list")
+# 
+# newgrades=[]
+# i=0
+# while i < len(students):
+#     print(students[i])
+#     if students[i] >= 10:
+#         newgrades.append(students[i])
+#     i += 1
+# print(newgrades)
+# 
+# missingGrade = int(input("please enter in your grade"))
+# newgrades.append(missingGrade)
+# print(newgrades)
+# newgrades.sort()
+# newgrades.reverse()
+# print(newgrades)
+
+
+
+#task 3
+password=("password123")
+while password != answer:
+    answer = input("what is your password")
+    print("incorect try again")
+    else:
+        print("corect entry granted")
+
+  
+    
+    
